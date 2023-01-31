807107de50a38570e7a19ada825569e89b64f1fb
IshikawaMasashi <IshikawaMasashi@outlook.com>
2023-01-31 15:45:12 +0900

    fix

--- src/strings.ts
+++ src/strings.ts
@@ -34,3 +34,31 @@ export function getTextBetweenParentheses(text: string) {
   }
   return result;
 }
+
+export function findOpenParen(text: string, closePos: number) {
+  let openPos = closePos;
+  let counter = 1;
+  while (counter > 0) {
+    const c = text[--openPos];
+    if (c == '(') {
+      counter--;
+    } else if (c == ')') {
+      counter++;
+    }
+  }
+  return openPos;
+}
+
+export function findClosingParen(text: string, openPos: number) {
+  let closePos = openPos;
+  let counter = 1;
+  while (counter > 0) {
+    const c = text[++closePos];
+    if (c == '(') {
+      counter++;
+    } else if (c == ')') {
+      counter--;
+    }
+  }
+  return closePos;
+}
